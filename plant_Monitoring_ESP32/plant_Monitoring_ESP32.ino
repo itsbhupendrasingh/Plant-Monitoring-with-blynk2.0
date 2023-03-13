c8d1416151ee475f611969697cf0c1a69ad363e8
Bhupendra Singh <itsbhupendrasingh@gmail.com>
2023-03-13 23:19:56 +0530

    Add files via upload

---
+++ plant_Monitoring_ESP32/plant_Monitoring_ESP32.ino
@@ -0,0 +1,66 @@
+
+
+#define BLYNK_TEMPLATE_ID "Template ID"
+#define BLYNK_TEMPLATE_NAME "Template ID"
+#define BLYNK_AUTH_TOKEN "Auth Token"
+
+#define BLYNK_PRINT Serial
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+
+char auth[] = BLYNK_AUTH_TOKEN;
+char ssid[] = "**********";
+char pass[] = "**********";
+
+BlynkTimer timer;
+
+#define soil 13
+#define waterPump 34
+WidgetLED pump(V1);
+
+float soilLevel, soilR;
+WidgetLCD vLcd(V2);
+void SMESensor()
+{
+  readSoil();
+  if(soilLevel>=80)
+  {    
+    digitalWrite(waterPump, LOW);
+    vLcd.print(0,0,"Water Pump OFF");
+    pump.off();
+  }
+  
+ if(soilLevel<20)
+  {    
+    digitalWrite(waterPump, HIGH);
+    vLcd.print(0,0, "Water Pump ON ");
+    pump.on();
+  }
+}
+void readSoil()
+{
+  soilR=analogRead(soil);  
+  Serial.println(soilR);
+  soilLevel=map(soilR, 0, 1024, 200,0); // adjust soil level here
+  Serial.println(soilLevel);
+  Blynk.virtualWrite(V0, soilLevel);
+  delay(1000);
+}
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(waterPump, OUTPUT);
+  digitalWrite(waterPump, LOW);
+  delay(100);
+
+  Blynk.begin(auth, ssid, pass);
+  delay(1000);
+  timer.setInterval(1000L, SMESensor);
+}
+
+void loop() {
+  Blynk.run();
+  timer.run();
+}
